a593e88fff113643539e3520679680b3288fe5d1
wtarit <60599564+wtarit@users.noreply.github.com>
2026-01-09 15:36:41 -0800

    Mermaid diagram

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mermaid({
+      theme: "forest",
+      autoTheme: true,
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
